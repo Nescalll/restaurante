cdb0f01be1a64a22086ddd09ae3e3560f72504d0
Nescalll <joao.vitor.2007.10.08@gmail.com>
2025-06-30 17:45:16 -0300

    Terminos das ações dos funcionarios

---
+++ movimento.py
@@ -0,0 +1,44 @@
+import sqlite3
+conn = sqlite3.connect('restaurante.db')
+cursor = conn.cursor()
+
+def visualizarTabela(nomeDaTabela, colunas = []):
+    conn = sqlite3.connect('restaurante.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM "+nomeDaTabela)
+    dados = cursor.fetchall()
+    voltas = 0
+    for i in dados:
+        for j in colunas:
+            print(j+":",str(i[voltas])+"; ", end="")
+            voltas += 1
+        voltas = 0
+        print()
+    return
+def validarEntradaDeFuncionario(nome, senha):
+    cursor.execute('SELECT * FROM funcionario')
+    dados = cursor.fetchall()
+    for info in dados:
+        nomeConfirmado = str.lower(info[0]) == str.lower(nome)
+        senhaConfirmado = int(info[3]) == senha
+        if (nomeConfirmado and senhaConfirmado):
+            return True
+    return False
+def validarExistencia(tabela, coluna, valor):
+    cursor.execute("SELECT "+coluna+" FROM "+tabela)
+    tabelaDeNomes = cursor.fetchall()
+    for i in tabelaDeNomes:
+        if(valor.upper() in i):
+            return True
+    return False
+def alterarDado(tabela, coluna, valores, colunaChave, valorChave):
+    for i in range(len(coluna)):
+        cursor.execute("UPDATE "+tabela+" SET "+coluna[i]+" = '"+valores[i]+"' WHERE "+colunaChave+" = '"+valorChave+"'")
+        conn.commit()
+    return
+def validarImportncia(senha):
+    cursor.execute('SELECT senha FROM funcionario WHERE funcao = "CHEFE"')
+    senhaReal = cursor.fetchall()
+    if(senha in senhaReal[0]):
+        return True
+    return False
--- pograma.py
+++ pograma.py
@@ -1,4 +1,5 @@
 import sqlite3
+import movimento as movimento
 
 conn = sqlite3.connect('restaurante.db')
 cursor = conn.cursor()
@@ -8,14 +9,7 @@ while (key == False):
     nome = input('informe o nome do usuário:')
     senha = int(input('informe a senha do usuário:'))
 #confirmar se o usuário e a senha existe
-    cursor.execute('SELECT * FROM funcionario')
-    dados = cursor.fetchall()
-    for info in dados:
-        nomeConfirmado = str.lower(info[0]) == str.lower(nome)
-        senhaConfirmado = info [3] == senha
-        if (nomeConfirmado and senhaConfirmado):
-            key = True
-            break
+    key = movimento.validarEntradaDeFuncionario(nome, senha)
     if(key == False):
         print('nome ou senha errado, comfirme se alguns deles esta errado e tente novamete')
 while (key == True):
@@ -27,24 +21,48 @@ while (key == True):
             funcionario = int(input('[1]ver funcionarios \n[2]alterar funcionario \n[3] adicionar/excluir funcionario \n:'))
             match funcionario:
                 case 1:
-                    cursor.execute('SELECT * FROM funcionario')
-                    dados = cursor.fetchall()
-
-                    for i in dados:
-                        print('funcionario:', i[0],'renda:', i[1],'função:', i[2])
+                    movimento.visualizarTabela("funcionario", ["funcionario", "salario", "função", "senha"])
                 case 2:
                     print('apenas funcionarios especificos podem acessar essa área. Para isso é necessario uma senha especial.')
-                    verificacao = int(input('digite a senha:'))
-                    cursor.execute('SELECT senha FROM funcionario WHERE funcao = "CHEFE"')
-                    senha = cursor.fetchall()
-                    match verificacao == senha:
+                    verificacao = input('digite a senha:')
+                    match movimento.validarImportncia(verificacao):
                         case False:
                             print('senha incorreta')
-                    match senha:
                         case True:
-                            pass
+                            print("informe o funcionario, e a nova renda e sua nova função separados por um espaço.")
+                            print("caso não queira mudar nada, basta repetir a função/renda")
+                            [funcionario, salario, funcao] = input().split()
+                            funcionarioExitente = movimento.validarExistencia("funcionario", "nome", funcionario)
+                            match funcionarioExitente:
+                                case False:
+                                    print("O funcionario não existe")
+                                case True:
+                                    movimento.alterarDado("funcionario", ["salario", "funcao"], [salario, funcao.upper()], "nome", funcionario.upper())
+                                    print("usurio adicionado")
                 case 3:
-                    pass
+                    acaoDoUsuario = int(input("O que deseja? \n[1]criar \n[2]excluir \n"))
+                    print('apenas funcionarios especificos podem acessar essa área. Para isso é necessario uma senha especial.')
+                    verificacao = input('digite a senha:')
+                    match movimento.validarImportncia(verificacao):
+                        case False:
+                            print('senha incorreta')
+                        case True:
+                            match acaoDoUsuario:
+                                case 1:
+                                    print("Para adicionar um novo funcionario")
+                                    print("informe o nome, salario, funcao e senha do novo funcionario")
+                                    [nome, salario, funcao, senha] = input(":").split()
+                                    cursor.execute("INSERT INTO funcionario (nome, salario, funcao, senha) VALUES (?,?,?,?)", (nome, salario, funcao, senha))
+                                    conn.commit()
+                                case 2:
+                                    nome = input("Digite o nome do usuario \n:")
+                                    match movimento.validarExistencia(nome):
+                                        case True:
+                                            cursor.execute("DELETE FROM funcionario WHERE nome = '"+nome+"'")
+                                            conn.commit()
+                                            print("O funcionario foi excluido")
+                                        case False:
+                                            print("Funcionario não existe")
                 case _:
                     print("Ação invalida")
 
@@ -53,21 +71,22 @@ while (key == True):
             cliente = int(input('[1] ver cliente \n[2]alterar cliente \n[3]adicionar/excluir cliente \n:'))
             match cliente:
                 case 1:
-                    cursor.execute('SELECT * FROM cliente')
-                    dados = cursor.fetchall()
-
-                    for i in dados:
-                        print('cliente:',i[0],'nome:',i[1],'historico:')
+                    movimento.visualizarTabela("cliente", ["nome"])
+                case _:
+                    print("Ação invalida")
 
 #categoria de produtos.
         case 3:
             produtos= int(input('[1] ver produtos \n[2]alterar produtos \n[3]adicionar/exluir produtos \n:'))
             match produtos:
                 case 1:
-                    cursor.execute('SELECT * FROM produtos')
+                    movimento.visualizarTabela("produto", ["nome", "preco", "quantidade","validade"])
+                case _:
+                    print("Ação invalida")                    
 
 #sair do programa.
         case 4:
             key = False
+            conn.close()
         case _:
             print('não existe essa função.\n tente novamente!')
